5fb0a7c57db3eb11d9c9f41e11542d7383eef922
Vitrana-reports <123052373+Vitrana-reports@users.noreply.github.com>
2023-07-03 13:24:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2112,10 +2112,10 @@ resource "aws_wafv2_web_acl" "main" {
             }
             positional_constraint = lookup(byte_match_statement.value, "positional_constraint")
             search_string         = lookup(byte_match_statement.value, "search_string")
-            text_transformation {
+#            text_transformation {
 #              priority = lookup(byte_match_statement.value["text_transformation"], "priority")
 #              type     = lookup(byte_match_statement.value["text_transformation"], "type")
-            }
+#           }
           }
         }
 
